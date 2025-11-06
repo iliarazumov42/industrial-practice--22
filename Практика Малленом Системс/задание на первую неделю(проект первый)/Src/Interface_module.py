b7564d2139db9a222d2a09e17fe6b3692bb29713
Разумов Илья <ilarazumov739@gmail.com>
2025-11-06 18:43:16 +0300

    внесение изменений

--- Практика Малленом Системс/задание на первую неделю(проект первый)/Src/Interface_module.py
+++ Практика Малленом Системс/задание на первую неделю(проект первый)/Src/Interface_module.py
@@ -1,6 +1,6 @@
 import modification_module
 
-print("Выберите действие:")
+print("Выберите нужную вам опирацию:")
 print("1 - Изменить размер изображения")
 print("2 - Повернуть изображение")
 
@@ -8,12 +8,12 @@ user_selection = input("Введите 1 или 2: ")
 picture_way = input("Введите путь к изображению: ")
 
 if user_selection == "1":
-    width = int(input("Введите новую ширину: "))
-    length = int(input("Введите новую высоту: "))
+    width = int(input("Введите желаемую ширину: "))
+    length = int(input("Введите желаемую длинну: "))
     result_way = modification_module.resize_picture(picture_way, width, length)
-    print('Сохранено в папку Python313: {result_way}')
+    print('Сохранено в папку с модулем: {result_way}')
 if user_selection == "2":
     corner = int(input('Введите угол поворота (например, 90): '))
     result_way = modification_module.rotate_picture(picture_way, corner)
-    print('Сохранено в папку Python313: {result_way}')
+    print('Сохранено в папку с модулем: {result_way}')
 
